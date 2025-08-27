415ec66f5a376b241ab2e9c1e3c88919b1e14816
Vivan Kumar <vivan2003.vk@gmail.com>
2025-08-27 17:20:55 +0530

    Added functions in service layer

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT,
-    SALT: process.env.SALT
+    SALT: process.env.SALT,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
 const { UserRepository } = require("../repositories/index");
+const { JWT_SECRET_KEY } = require("../config/server-config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 class UserService {
     constructor() {
@@ -24,6 +27,40 @@ class UserService {
             throw { error };
         }
     }
+
+    createToken = (user) => {
+        try {
+            const token = jwt.sign(user, JWT_SECRET_KEY, {
+                expiresIn: "1h"
+            })
+            return token;
+        } catch (error) {
+            console.log("something went wrong in the token creation");
+            throw { error };
+        }
+    }
+
+    validateToken = (token) => {
+        try {
+            const response = jwt.verify(token, JWT_SECRET_KEY);
+            return response;
+        } catch (error) {
+            console.log("something went wrong in the token validation");
+            throw { error };
+        }
+    }
+
+    comparePassword = (userInputplainTextPassword, encryptedPassword) => {
+        try {
+            const response = bcrypt.compareSync(userInputplainTextPassword, encryptedPassword);
+            return response;
+        } catch (error) {
+            console.log("something went wrong while comparing password");
+            throw { error };
+        }
+    }
+
+
 }
 
 module.exports = UserService;
